27204b1122681a4bbea7859945be9004839a0587
Jason Adam Young <jasonadamyoung@gmail.com>
2012-05-01 16:37:41 -0400

    local list and repost functionality.

--- lib/capatross/version.rb
+++ lib/capatross/version.rb
@@ -1,3 +1,3 @@
 module Capatross
-  VERSION = "0.3.0"
+  VERSION = "0.4.0"
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,11 @@
 # see LICENSE file
 require 'thor'
 require 'yaml'
+require 'json'
 require 'capatross/version'
+require 'capatross/options'
+require 'capatross/deep_merge' unless defined?(DeepMerge)
+require 'httparty'
 module Capatross
   class CLI < Thor
     include Thor::Actions
@@ -66,7 +70,69 @@ module Capatross
       copy_configs
       add_local_to_gitignore
       add_capatross_to_deploy
-    end        
+    end
+    
+    desc "list", "list local deploys"
+    #method_option :source, :default => 'local', :aliases => "-s", :desc => "Source of the d"
+    def list
+      logsdir = './capatross_logs'
+      if(!File.exists?(logsdir))
+         say("Error: Capatross log directory (#{logsdir}) not present", :red)
+      end
+      
+      deploy_logs = []
+      # loop through the files
+      Dir.glob(File.join(logsdir,'*.json')).sort.each do |logfile|
+        logdata = JSON.parse(File.read(logfile))
+        logdata.delete('deploy_log')
+        deploy_logs << logdata
+      end      
+      
+      deploy_logs.sort_by{|log| log['start']}.each do |log|
+        if(log['success'])
+          message = "#{log['capatross_id']} : Revision: #{log['deployed_revision']} deployed at #{log['start'].to_s} to #{log['location']}"
+        else
+          message = "#{log['capatross_id']} : Deploy failed at #{log['start'].to_s} to #{log['location']}"
+        end
+        
+        if(log['finish_posted'])
+          message += ' (posted)'
+          say(message)
+        else
+          message += ' (not posted)'
+          say(message,:yellow)
+        end
+      end
+    end
+    
+    desc "post", "post or repost the logdata from the specified local deploy"
+    #method_option :source, :default => 'local', :aliases => "-s", :desc => "Source of the d"
+    method_option :log, :aliases => '-l', :type => :string, :required => true, :desc => "The capatross deploy id to post/repost (use 'list' to show known deploys)"
+    def post
+      logfile = "./capatross_logs/#{options[:log]}.json"
+      if(!File.exists?(logfile))
+         say("Error: The specified capatross log (#{options[:log]}) was not found", :red)
+      end
+      logdata = JSON.parse(File.read(logfile))
+      
+      
+      settings = Capatross::Options.new
+      settings.load!
+      
+      result = HTTParty.post("#{settings.albatross_uri}#{settings.albatross_deploy_path}",
+                              :body => logdata,
+                              :headers => { 'ContentType' => 'application/json' })
+                              
+      if(result.response.code == '200')
+        say("Log data posted to #{settings.albatross_uri}#{settings.albatross_deploy_path}")
+        # update that we posted
+        logdata['finish_posted'] = true
+        File.open(logfile, 'w') {|f| f.write(logdata.to_json) }
+      else
+        say("Unable to post log data to #{settings.albatross_uri}#{settings.albatross_deploy_path} (Code: #{result.response.code })",:red)
+      end
+    end
+              
   end
   
 end
