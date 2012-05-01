feb171eea6419ea07c0f853ffb9e68a9eea53396
Jason Adam Young <jasonadamyoung@gmail.com>
2012-05-01 16:37:02 -0400

    rework capistrano tasks

--- lib/capatross/recipes/capatross.rb
+++ lib/capatross/recipes/capatross.rb
@@ -10,7 +10,7 @@
 
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :capatross do
-    
+      
     ## no descriptions for the following tasks - meant to be hooked by capistrano
     task :start do
       set(:whereto,capatross_core.whereto(self))
@@ -18,42 +18,32 @@ Capistrano::Configuration.instance(:must_exist).load do
                                       deployer_email: capatross_core.gitutils.user_email,
                                       deployer_name:  capatross_deployer,
                                       previous_revision: current_revision,
-                                      start: Time.now,
+                                      start: Time.now.utc,
                                       location: whereto)
       start_posted = capatross_core.post_deploydata
       capatross_core.merge_deploydata(start_posted: start_posted)
-      logger.info "#{capatross_deployer} is starting to deploy #{application} to #{whereto}"
     end
     
-    task :finish_success do
+    task :finish do
       set(:whereto,capatross_core.whereto(self))
-      capatross_core.merge_deploydata(deployed_revision: current_revision, success: true, finish: Time.now)
-      finish_posted = capatross_core.post_deploydata
-      capatross_core.merge_deploydata(finish_posted: finish_posted)
-      logger.info "#{capatross_deployer} successfully deployed #{application} to #{whereto}"
-    end
-    
-    task :finish_failure do
-      set(:whereto,capatross_core.whereto(self))
-      capatross_core.merge_deploydata(success: false)
-      finish_posted = capatross_core.post_deploydata
-      capatross_core.merge_deploydata(finish_posted: finish_posted)
-      logger.error "The deploy of #{application} to #{whereto} by #{capatross_deployer} failed."
-    end
-    
-    task :copy_log, :except => { :no_release => true} do
       logger = Capistrano::CapatrossLogger
+      if(logger.successful?)
+        capatross_core.merge_deploydata(deployed_revision: current_revision, success: true, finish: Time.now.utc)
+      else
+        capatross_core.merge_deploydata(success: false)
+      end
+      
       capatross_core.merge_deploydata(deploy_log: File.open(logger.log_file_path).read)
+      finish_posted = capatross_core.post_deploydata
+      capatross_core.merge_deploydata(finish_posted: finish_posted)
       outputfile = capatross_core.write_deploydata
       if(capatross_core.settings.copy_log_to_server)
         run "mkdir -p #{shared_path}/deploy_logs"
         put File.open(outputfile).read, "#{shared_path}/deploy_logs/#{File.basename(outputfile)}"
-      end
+      end      
     end
-          
+              
   end
 end
 
-Capistrano::CapatrossLogger.post_process_hook("capatross:finish_success",:success)
-Capistrano::CapatrossLogger.post_process_hook("capatross:finish_failure",:failure)
-Capistrano::CapatrossLogger.post_process_hook("capatross:copy_log",:any)
+Capistrano::CapatrossLogger.post_process_hook("capatross:finish",:any)
